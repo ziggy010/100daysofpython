3d6a201952a10e74063df10d42d8e77ee04a350f
ARETEE <tajale01@gmail.com>
2024-12-22 23:32:01 +0545

    day 3 commit

---
+++ Day 3/Day3.py
@@ -0,0 +1,6 @@
+print("\nWelcome to Python Pizza Deliveries!\n")
+
+size = input("\nWhat size pizza do you want? S, M OR L:\n- ")
+pepperoni = input("\nDo you want pepperoni on your pizza? Y or N:\n- ")
+extra_cheese = input("\nDo you want extra cheese? Y or N:\n- ")
+
