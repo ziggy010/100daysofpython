f9e8cfcc5106ad8130b51841af7c7ef173a3b7c0
ARETEE <tajale01@gmail.com>
2024-12-22 15:56:33 +0545

    day 2 commit

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,1 +1,24 @@
-print("hello"[0])
+# final project
+
+print("\nWelcome to the tip Calculator\n")
+
+total_bill = float(input("What was the total bill?\n- "))
+
+tip = int(input("\nHow much tip would you like to give? 10, 12 or 15?\n- "))
+
+people = int(input("\nHow many people to split the bill?\n- "))
+
+total_inlucding_tip = total_bill + (total_bill * (tip / 100));
+
+final_bill = total_inlucding_tip / people;
+
+print(f"Each person should pay: {round(final_bill,2)}");
+
+
+
+
+
+
+
+
+
