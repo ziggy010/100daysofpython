d2e6694cd9476ebf04e3c0af393c74b748a080e5
ARETEE <tajale01@gmail.com>
2024-12-24 16:04:15 +0545

    Day 5 commit

---
+++ Day 5/Day5.py
@@ -0,0 +1,19 @@
+# student_scores= [14,156,131,124,1345,132,454];
+
+# print(sum(student_scores));
+
+# number = 0;
+
+# for item in student_scores:
+#     if(item > number):
+#         number = item
+
+# print(number);
+
+# total = 0;
+
+# for item in range(1,101):
+#     total += item;
+
+# print(total);
+
