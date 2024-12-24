45b7581890d71eecad25cd22978c3e7411710dd7
ARETEE <tajale01@gmail.com>
2024-12-24 13:01:23 +0545

    day 4 commit

--- Day 4/Day4.py
+++ Day 4/Day4.py
@@ -1,2 +1,13 @@
-#starting day 4. 
+import random;
 
+# random_float = random.random() * 20;
+
+# print(round(random_float, 2));
+
+
+random_number = random.randint(1,2);
+
+if(random_number == 1):
+    print("Heads!")
+else:
+    print("Tails!")
