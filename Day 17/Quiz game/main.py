2a06a6c8b76c2d4088b4da17e69856efc7cc7482
ARETEE <tajale01@gmail.com>
2025-01-05 22:24:41 +0545

    day 17 final commit

---
+++ Day 17/Quiz game/main.py
@@ -0,0 +1,16 @@
+from question_model import Question;
+from data import question_data;
+from quiz_brain import QuizBrain;
+
+question_bank = [];
+
+for item in question_data:
+    question_bank.append(Question(text=item["text"], answer=item["answer"]));
+
+my_question = QuizBrain(q_list=question_bank);
+
+while my_question.still_has_questions():
+    my_question.next_question();
+
+
+    
---
+++ Day 17/Quiz game/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text.capitalize();
+        self.answer = answer;
+
