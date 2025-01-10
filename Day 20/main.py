fa530d0f26622733bfd9baec529db7d46f2a6fad
ARETEE <tajale01@gmail.com>
2025-01-10 10:11:23 +0545

    day 20

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen;
+import time;
 
 screen = Screen();
 
 screen.bgcolor('black');
 screen.setup(width=600, height=600);
 screen.title("My snake game!");
+screen.tracer(0);
 
 
 starting_position = [(0,0), (-20, 0), (-40, 0)];
@@ -18,11 +20,21 @@ for i in starting_position:
     snake.goto(i)
     snakes.append(snake);
 
+
 is_game_on = True;
 
 while is_game_on:
-    for item in snakes:
-        item.forward(20);
+    screen.update();
+    time.sleep(0.2);
+    for item in range(len(snakes)-1,0,-1):
+        x_cor = snakes[item - 1].xcor();
+        y_cor = snakes[item - 1].ycor();
+        snakes[item].goto(x_cor, y_cor);
+    
+
+
+
+
 
 
 
